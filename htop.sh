99d7138a81840fc0fa25a963c7d91505b845c59f
Moustafa Meshry <mmeshry@vulcansub00.umiacs.umd.edu>
2017-05-31 17:34:21 -0400

    htop and tmux installation

---
+++ htop.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+#-------------------------------------------------------------------------------
+# Script: Install htop, without root access
+# Author: Moustafa Meshry
+# Date  : May 31, 2017
+#-------------------------------------------------------------------------------
+
+# exit on error
+set -e
+
+HTOP_VERSION=2.0.1
+
+# create our directories
+mkdir -p $HOME/local $HOME/htop_tmp
+cd $HOME/htop_tmp
+
+# Install libncurses, if not already there
+if ! ldconfig -p | grep libncurses > /dev/null; then
+    echo "Installing libncurses..."
+    tar xvzf ncurses-5.9.tar.gz
+    cd ncurses-5.9
+    ./configure --prefix=$HOME/local
+    make
+    make install
+    cd ..
+fi
+
+# Install htop if not already there
+if ! which htop > /dev/null 2> /dev/null; then
+    wget -O htop-${HTOP_VERSION}.tar.gz http://hisham.hm/htop/releases/${HTOP_VERSION}/htop-${HTOP_VERSION}.tar.gz
+    tar xvfvz htop-${HTOP_VERSION}.tar.gz
+    cd htop-${HTOP_VERSION}
+    ./configure --prefix=$HOME/local
+    make
+    make install
+    cd ..
+fi
+
+# cleanup
+rm -rf $HOME/htop_tmp
+
+# add $HOME/local/bin to $PATH
+if ! which htop > /dev/null 2> /dev/null; then
+    echo -e "\n# add \$HOME/local/bin to \$PATH\nexport PATH=\"$HOME/local/bin:\$PATH\"\n" >> ~/.bashrc
+fi
+
---
+++ tmux.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+#-------------------------------------------------------------------------------
+# Script         : Install htop, without root access
+# Original script: https://gist.github.com/ryin/3106801
+# Modified by    : Moustafa Meshry
+# Date           : May 31, 2017
+# It's assumed that wget and a C/C++ compiler are installed.
+#-------------------------------------------------------------------------------
+
+# exit on error
+set -e
+
+TMUX_VERSION=2.5
+
+# create our directories
+mkdir -p $HOME/local $HOME/tmux_tmp
+cd $HOME/tmux_tmp
+
+# Install libevent, if not already there
+if ! ldconfig -p | grep libevent > /dev/null; then
+    wget https://github.com/downloads/libevent/libevent/libevent-2.0.19-stable.tar.gz
+    echo "Installig libevent..."
+    tar xvzf libevent-2.0.19-stable.tar.gz
+    cd libevent-2.0.19-stable
+    ./configure --prefix=$HOME/local --disable-shared
+    make
+    make install
+    cd ..
+fi
+
+# Install libncurses, if not already there
+if ! ldconfig -p | grep libncurses > /dev/null; then
+    wget ftp://ftp.gnu.org/gnu/ncurses/ncurses-5.9.tar.gz
+    echo "Installing libncurses..."
+    tar xvzf ncurses-5.9.tar.gz
+    cd ncurses-5.9
+    ./configure --prefix=$HOME/local
+    make
+    make install
+    cd ..
+fi
+
+# Install tmux if not already there
+if ! which tmux > /dev/null 2> /dev/null; then
+    wget -O tmux-${TMUX_VERSION}.tar.gz https://github.com/tmux/tmux/releases/download/${TMUX_VERSION}/tmux-${TMUX_VERSION}.tar.gz
+    tar xvzf tmux-${TMUX_VERSION}.tar.gz
+    cd tmux-${TMUX_VERSION}
+    ./configure CFLAGS="-I$HOME/local/include -I$HOME/local/include/ncurses" LDFLAGS="-L$HOME/local/lib -L$HOME/local/include/ncurses -L$HOME/local/include"
+    CPPFLAGS="-I$HOME/local/include -I$HOME/local/include/ncurses" LDFLAGS="-static -L$HOME/local/include -L$HOME/local/include/ncurses -L$HOME/local/lib" make
+    cp tmux $HOME/local/bin
+    cd ..
+fi
+
+# cleanup
+rm -rf $HOME/tmux_tmp
+
+# add $HOME/local/bin to $PATH
+if ! which tmux > /dev/null 2> /dev/null; then
+    echo -e "\n# add \$HOME/local/bin to \$PATH\nexport PATH=\"$HOME/local/bin:\$PATH\"\n" >> ~/.bashrc
+fi
